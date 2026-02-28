5907af491a0069df12c1be45f6fe2427f784a8a5
m.inyushkin <m.inyushkin@punkt-e.ru>
2026-02-28 15:45:06 +0300

    LIC

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from carbon!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="GreenTrace",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "psutil>=7.2.2",
+        "py-cpuinfo>=9.0.0",
+    ],
+    description="Effortless CO₂ Emissions Tracking for Python Code",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/so89bur/GreenTrace",
+    author="Sov Bur",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
